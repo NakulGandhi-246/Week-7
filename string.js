f200a9b853e79e2a77bf784e0a935ad63b729faf
NakulGandhi-246 <gandhi.nakul@tftus.com>
2025-09-05 15:59:47 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,11 @@
+let name = 'Nakul';
+console.log(name.length);
+console.log(name);
+
+let boy1 = 'Rohit';
+let boy2 = 'Parkash';
+let sentence =`${boy1} is a friend of ${boy2}`;
+console.log(sentence);
+
+console.log(boy1.concat(boy2));
+console.log(sentence.split(''));
